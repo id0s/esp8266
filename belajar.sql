f5e61be394033df264ea6c5e754b92c3b642ab35
i do's <136927932+id0s@users.noreply.github.com>
2024-05-16 23:29:36 -0700

    mentahan database

---
+++ belajar.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 02, 2019 at 10:15 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `belajar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `perintah`
+--
+CREATE DATABASE belajar;
+USE belajar;
+CREATE TABLE `perintah` (
+  `id_perintah` int(11) NOT NULL,
+  `data` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `perintah`
+--
+
+INSERT INTO `perintah` (`id_perintah`, `data`) VALUES
+(1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sensor`
+--
+
+CREATE TABLE `sensor` (
+  `id_sensor` int(11) NOT NULL,
+  `data_sensor` decimal(10,2) NOT NULL,
+  `waktu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sensor`
+--
+
+INSERT INTO `sensor` (`id_sensor`, `data_sensor`, `waktu`) VALUES
+(1, '15.00', 1563836562),
+(2, '10.00', 1563836593),
+(3, '10.33', 1563836609),
+(4, '9.00', 1563836716),
+(5, '8.00', 1563837521);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `perintah`
+--
+ALTER TABLE `perintah`
+  ADD PRIMARY KEY (`id_perintah`);
+
+--
+-- Indexes for table `sensor`
+--
+ALTER TABLE `sensor`
+  ADD PRIMARY KEY (`id_sensor`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `perintah`
+--
+ALTER TABLE `perintah`
+  MODIFY `id_perintah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `sensor`
+--
+ALTER TABLE `sensor`
+  MODIFY `id_sensor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
